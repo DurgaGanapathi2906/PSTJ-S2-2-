2515d8f135dc4a6a9933aa333f8a10fbd8ca3f6d
Varjaparti Durga Ganapathi <vtu28832@veltech.edu.in>
2026-07-21 12:19:20 +0530

    Add binary search implementation in BinarySearch.java
    Implemented binary search algorithm to find an element in a sorted array.

---
+++ Task-2 Search for a given element in a sorted array using binary search/BinarySearch.java
@@ -0,0 +1,25 @@
+//Code
+import java.util.Scanner;
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int key = sc.nextInt();
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == key) {
+                System.out.println("Found at index " + mid);
+                return;
+            }
+            if (key < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        System.out.println("Not Found");
+    }
+}
+
+//Output
+60
