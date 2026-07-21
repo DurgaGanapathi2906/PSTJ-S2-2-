38ec28a0f7355300d87b32728f6ffa231007a07f
Varjaparti Durga Ganapathi <vtu28832@veltech.edu.in>
2026-07-21 12:20:56 +0530

    Add MaximumElement class to find max in array
    Implement a program to find the maximum element in an array of integers.

---
+++ Task-3 Find the maximum Element in an array of 'n' integers/MaximumElement.java
@@ -0,0 +1,17 @@
+//Code
+public class MaximumElement {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 8, 67, 23, 91, 34};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum element = " + max);
+    }
+}
+
+//Output
+60
+Found at index 5
