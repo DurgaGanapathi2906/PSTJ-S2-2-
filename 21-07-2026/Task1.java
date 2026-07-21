6293eabd563ab5f9b801d6a9d1b9cf51caedc020
Varjaparti Durga Ganapathi <vtu28832@veltech.edu.in>
2026-07-21 12:32:48 +0530

    Add output comment for indexArray program
    Added output comment to display the expected result.

---
+++ 21-07-2026/Task1.java
@@ -0,0 +1,12 @@
+//Problem Statement: Access and print the elements at a given index in an array
+//Code
+public class indexArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println(numbers[2]);
+    }
+}
+
+//Output
+30
