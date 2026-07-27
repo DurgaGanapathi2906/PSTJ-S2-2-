c92ee867083b4e4a6bc5ed543f61a5082a70d316
Varjaparti Durga Ganapathi <vtu28832@veltech.edu.in>
2026-07-27 12:18:48 +0530

    Add Running Sum of 1D Array solution

---
+++ 27-07-2026/Week1/Session 4/Running Sum of 1D Array.java
@@ -0,0 +1,13 @@
+//Code:
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
+
+//Output:
+Accepted
+54 / 54 testcases passed
